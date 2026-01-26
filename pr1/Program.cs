6f057f4f3c4b9d6477b22c417a592a855c78b728
pigeonIlesh <Ilyanikulin0820@gmail.com>
2026-01-26 16:46:06 +0300

    ghgh

---
+++ pr1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "моя_биография.txt";
+            using (StreamWriter wr = new StreamWriter(file))
+            {
+                wr.WriteLine($"- Меня зовут Иван");
+                wr.WriteLine($"- Мне 20 лет");
+                wr.WriteLine($"- Я учусь программированию");
+                wr.WriteLine($"- Мое хобби Brawl Stars");
+                wr.WriteLine($"- Мечтаю стать разработчиком");
+            }
+            using (StreamReader sr = new StreamReader(file))
+            {
+                int koll = 1;
+                string line;
+                //Читаем строку за строкой, пока не достигнем конца
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine($"Строка {koll}: {line}");
+                    koll++;
+                }
+            }
+            int kol = 1;
+            using (StreamReader sr = new StreamReader(file))
+            {
+                string line = sr.ReadLine();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    kol++;
+                }
+            }
+            Console.WriteLine($"Общее число строк в документе = {kol}");
+        }
+    }
+}
---
+++ pr2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\232415\\Documents\\Дневник\\Дневник настроений.txt";
+            DateTime now = DateTime.Now;
+            Console.WriteLine("ДНЕВНИК НАСТРОЕНИЙ");
+            Console.WriteLine(now.ToString("dd.MM.yyyy"));
+            Console.Write($"Оцените свое настроение (1-5): ");
+            int nas = int.Parse(Console.ReadLine());
+            Console.Write($"Введите краткий комментарий: ");
+            string kom = Console.ReadLine();
+            using (StreamWriter sw = new StreamWriter(file, true))
+            {
+                sw.WriteLine($"[{now.ToString("dd.MM.yyyy")}] Настроение: {nas}/5 - {kom}");
+                Console.WriteLine($"Запись добалена");
+            }
+        }
+    }
+}
